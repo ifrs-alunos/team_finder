ccac7bbdda7d0025bf3e01f8f88d28c0d6c9f2bd
Diego Cardoso Nunes <diegocar.nunes@gmail.com>
2019-10-28 21:07:26 -0300

    Fixed migrations conflicts. Configured environ to work with 'local settings'

--- team_finder/team_finder/settings.py
+++ team_finder/team_finder/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'team_finder',
+    'accounts',
+    'tinder',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+from .local_settings import *
